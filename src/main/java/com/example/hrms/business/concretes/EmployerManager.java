50e67d8c32e683cb3da4b884a66002cf2b5f31da
Mine ÇAKIR <cakirrmine00@gmail.com>
2022-08-04 14:36:56 +0300

    Added EmployerService and EmployerManager

---
+++ src/main/java/com/example/hrms/business/concretes/EmployerManager.java
@@ -0,0 +1,36 @@
+package com.example.hrms.business.concretes;
+
+import com.example.hrms.business.abstracts.EmployerService;
+import com.example.hrms.core.utilities.results.DataResult;
+import com.example.hrms.core.utilities.results.Result;
+import com.example.hrms.core.utilities.results.SuccessDataResult;
+import com.example.hrms.core.utilities.results.SuccessResult;
+import com.example.hrms.dataAccess.abstracts.EmployerDao;
+import com.example.hrms.entities.concretes.Employer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployerManager implements EmployerService {
+    private EmployerDao employerDao;
+
+    @Autowired
+    public EmployerManager(EmployerDao employerDao)
+    {
+        this.employerDao = employerDao;
+    }
+
+    @Override
+    public DataResult<List<Employer>> getAll() {
+        return new SuccessDataResult<List<Employer>>(this.employerDao.findAll(),"Employers are listed.");
+    }
+
+    @Override
+    public Result add(Employer employer) {
+        this.employerDao.save(employer);
+        return new SuccessResult("Employer is added.");
+    }
+
+}
---
+++ src/main/java/com/example/hrms/dataAccess/abstracts/EmployerDao.java
@@ -0,0 +1,8 @@
+package com.example.hrms.dataAccess.abstracts;
+
+import com.example.hrms.entities.concretes.Employer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployerDao extends JpaRepository<Employer,Integer> {
+
+}
