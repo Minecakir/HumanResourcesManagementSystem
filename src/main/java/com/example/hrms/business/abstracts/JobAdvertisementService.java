f0b1f383ab48b764df1d802e90720c62c147e04c
Mine ÇAKIR <cakirrmine00@gmail.com>
2022-08-07 13:05:30 +0300

    Added JobAdvertisementService

---
+++ src/main/java/com/example/hrms/business/abstracts/JobAdvertisementService.java
@@ -0,0 +1,16 @@
+package com.example.hrms.business.abstracts;
+
+import com.example.hrms.core.utilities.results.DataResult;
+import com.example.hrms.core.utilities.results.Result;
+import com.example.hrms.entities.concretes.JobAdvertisement;
+
+import java.util.List;
+
+public interface JobAdvertisementService {
+    DataResult<List<JobAdvertisement>> getAll();
+    Result add(JobAdvertisement jobAdvertisement);
+    DataResult<List<JobAdvertisement>> getAllActive();
+    DataResult<List<JobAdvertisement>> findByIsActiveTrueOrderByPublishDate();
+    DataResult<List<JobAdvertisement>> findByIsActiveTrueAndEmployer_CompanyName(String companyName);
+
+}
