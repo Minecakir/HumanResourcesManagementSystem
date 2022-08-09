9e5615189ea41786f706a1cd7e55c74404ec5a14
Mine ÇAKIR <cakirrmine00@gmail.com>
2022-08-09 20:32:38 +0300

    Created Cv

---
+++ src/main/java/com/example/hrms/entities/concretes/Cv.java
@@ -0,0 +1,23 @@
+package com.example.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cvs")
+public class Cv {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name="candidate_id")
+    private Candidate candidate;
+}
