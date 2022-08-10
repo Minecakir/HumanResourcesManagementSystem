2afed4d5399a0df3e646c73bbbdd6bd8d8418909
Mine ÇAKIR <cakirrmine00@gmail.com>
2022-08-10 12:06:23 +0300

    Created JobExperienceService

---
+++ src/main/java/com/example/hrms/business/abstracts/JobExperienceService.java
@@ -0,0 +1,12 @@
+package com.example.hrms.business.abstracts;
+
+import com.example.hrms.core.utilities.results.DataResult;
+import com.example.hrms.core.utilities.results.Result;
+import com.example.hrms.entities.concretes.JobExperience;
+
+import java.util.List;
+
+public interface JobExperienceService {
+    DataResult<List<JobExperience>> getAll();
+    Result add(JobExperience jobExperience);
+}
