814c54f2473a28ebcf0d64da6ca5267ec227597a
Mine ÇAKIR <cakirrmine00@gmail.com>
2022-08-10 21:14:00 +0300

    Created LanguageForCvsController

---
+++ src/main/java/com/example/hrms/api/controllers/LanguageForCvsController.java
@@ -0,0 +1,27 @@
+package com.example.hrms.api.controllers;
+
+import com.example.hrms.business.abstracts.LanguageForCvService;
+import com.example.hrms.core.utilities.results.DataResult;
+import com.example.hrms.core.utilities.results.Result;
+import com.example.hrms.entities.concretes.LanguageForCv;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/languagesfor_cv")
+public class LanguageForCvsController {
+    private LanguageForCvService languageForCvService;
+    @Autowired
+    public LanguageForCvsController(LanguageForCvService languageForCvService){this.languageForCvService = languageForCvService;}
+
+    @GetMapping("/getAll")
+    DataResult<List<LanguageForCv>> getAll(){return this.languageForCvService.getAll();};
+
+    @PostMapping("/add")
+    Result add(LanguageForCv languageForCv){return this.languageForCvService.add(languageForCv);};
+}
