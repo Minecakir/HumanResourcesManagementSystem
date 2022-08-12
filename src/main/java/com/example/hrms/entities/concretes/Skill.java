02e74a4a4179c0409bc0839acda8f2d88cda6342
Mine ÇAKIR <cakirrmine00@gmail.com>
2022-08-12 15:33:27 +0300

    Created SkillDao

---
+++ src/main/java/com/example/hrms/entities/concretes/Skill.java
@@ -0,0 +1,33 @@
+package com.example.hrms.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "skills")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","cv"})
+
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "skill_name")
+    private String skillName;
+
+    @ManyToOne
+    @JoinColumn(name = "cv_id")
+    private Cv cv;
+}
